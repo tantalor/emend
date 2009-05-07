5d44179ddd5765f6667d01db919419175df9f96f
John Tantalo <john.tantalo@gmail.com>
2009-05-07 09:58:14 -0700

    fixed case where 'from' parameter doesn't match a site

--- app/sites/default.py
+++ app/sites/default.py
@@ -4,9 +4,9 @@ PAGE_SIZE = 10
 def get(handler, response):
   # get some sites
   from_domain = handler.request.get('from')
-  if from_domain:
-    from_site = Site.all().\
-      filter('domain =', from_domain).get()
+  from_key_name = Site.key_name_from_domain(from_domain)
+  from_site = Site.get_by_key_name(from_key_name)
+  if from_site:
     sites = Site.all().\
       filter('open =', from_site.open).\
       filter('domain >=', from_domain).\
