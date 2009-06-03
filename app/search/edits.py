2df94a144bb379f8e52f1813af1542202653580f
John Tantalo <john.tantalo@gmail.com>
2009-06-03 16:41:41 -0700

    added search pagination

--- app/search/edits.py
+++ app/search/edits.py
@@ -1,6 +1,30 @@
 from model.edit import Edit
 
+PAGE_SIZE = 10
+
 def get(handler, response):
   query = handler.request.get('q')
   response.query = query
-  response.edits = list(Edit.all().search(query))
+  # get some edits
+  from_key = handler.request.get('from')
+  if from_key:
+    from_edit = Edit.get(from_key)
+  else:
+    from_edit = None
+  if from_edit:
+    edits = Edit.all().\
+      search(query).\
+      order('-created').\
+      filter('created <=', from_edit.created).\
+      fetch(PAGE_SIZE+1)
+  else:
+    edits = Edit.all().\
+      search(query).\
+      order('-created').\
+      fetch(PAGE_SIZE+1)
+  # for output
+  response.edits = edits[:PAGE_SIZE]
+  
+  if len(edits) > PAGE_SIZE:
+    response.has_next = 1
+    response.next_from = edits[PAGE_SIZE].key()
