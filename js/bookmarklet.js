e54598dbbc1efac5a7e7334fdb37c1363979c4cb
John Tantalo <john.tantalo@gmail.com>
2009-06-05 13:21:36 -0700

    added encodeURIComponent and version number

--- js/bookmarklet.js
+++ js/bookmarklet.js
@@ -1,12 +1,12 @@
 function()
 {
   window.open(
-    'http://emend.appspot.com?url='+location.href+'&original='+
-      (
+    'http://emend.appspot.com?url='+location.href+'&v=2&original='+
+      encodeURIComponent(
         window.getSelection ? window.getSelection() :
         document.getSelection ? document.getSelection() :
         document.selection ? document.selection.createRange().text :
         ''
       ),
-    '_blank');      
+    '_blank');
 }
