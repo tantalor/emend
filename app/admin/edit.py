39aec78806010a57c7aa2314dd4226b3ea3e0be0
John Tantalo <john.tantalo@gmail.com>
2009-07-16 09:28:33 -0700

    change 'missing' tested to 'none'

--- app/admin/edit.py
+++ app/admin/edit.py
@@ -1,13 +1,11 @@
 from model.edit import Edit
 
+import logging
+
 def get(handler, response):
   if not handler.is_admin():
     return handler.not_found(status=403)
   for edit in Edit.all():
-    if hasattr(edit, 'closed'):
-      if edit.closed:
-        edit.status = 'closed'
-      else:
-        edit.status = 'open'
-      delattr(edit, 'closed')
-    edit.put()
+    if not edit.tested:
+      edit.tested = None
+      edit.put()
