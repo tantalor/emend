5f973cabf8c5b70dd1ca53438f29e75db47dea7b
John Tantalo <john.tantalo@gmail.com>
2009-07-16 10:10:32 -0700

    removed admin requirement

--- app/tasks/edits/test.py
+++ app/tasks/edits/test.py
@@ -5,8 +5,6 @@ from google.appengine.api.urlfetch_errors import DownloadError
 FETCH_COUNT = 10
 
 def get(handler, response):
-  if not handler.is_admin():
-    return handler.not_found(status=403)
   # get edits in need of testing
   edits = Edit.all()\
     .filter('status =', 'open')\
