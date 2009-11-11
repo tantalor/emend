ac9bdd0f7bb527dff444a7e12db9fe6f262d596a
John Tantalo <john.tantalo@gmail.com>
2009-11-11 10:34:45 -0800

    added search by url

--- app/search/edits.py
+++ app/search/edits.py
@@ -1,3 +1,4 @@
+import re
 from model.edit import Edit
 
 PAGE_SIZE = 10
@@ -5,26 +6,47 @@ PAGE_SIZE = 10
 def get(handler, response):
   query = handler.request.get('q')
   response.query = query
-  # get some edits
+  # from edit
   from_key = handler.request.get('from')
   if from_key:
     from_edit = Edit.get(from_key)
   else:
     from_edit = None
-  if from_edit:
-    edits = Edit.all().\
-      search(query).\
-      order('-created').\
-      filter('created <=', from_edit.created).\
-      fetch(PAGE_SIZE+1)
+  # get some edits
+  if re.match('^http://', query):
+    edits = search_by_url(query, from_edit)
   else:
-    edits = Edit.all().\
-      search(query).\
-      order('-created').\
-      fetch(PAGE_SIZE+1)
+    edits = []#search_by_query(query, from_edit)
   # for output
   response.edits = edits[:PAGE_SIZE]
   
   if len(edits) > PAGE_SIZE:
     response.has_next = 1
     response.next_from = edits[PAGE_SIZE].key()
+
+def search_by_query(query, from_edit):
+  if from_edit:
+    return Edit.all().\
+      search(query).\
+      order('-created').\
+      filter('created <=', from_edit.created).\
+      fetch(PAGE_SIZE+1)
+  else:
+    return Edit.all().\
+      search(query).\
+      order('-created').\
+      fetch(PAGE_SIZE+1)
+
+
+def search_by_url(url, from_edit):
+  if from_edit:
+    return Edit.all().\
+      filter('url =', url).\
+      filter('created <=', from_edit.created).\
+      order('-created').\
+      fetch(PAGE_SIZE+1)
+  else:
+    return Edit.all().\
+      filter('url =', url).\
+      order('-created').\
+      fetch(PAGE_SIZE+1)
