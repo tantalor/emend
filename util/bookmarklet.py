aa7780914e834a2ee81bcb5ec4b698b14f513097
John Tantalo <john.tantalo@gmail.com>
2009-11-11 21:36:05 -0800

    added js/ to static dirs, added /local/js/ to read js/ locally

--- util/bookmarklet.py
+++ util/bookmarklet.py
@@ -1,6 +1,6 @@
 import re
 
 def bookmarklet():
-  bookmarklet = ''.join(file('js/bookmarklet.js').readlines())
+  bookmarklet = ''.join(file('local/js/bookmarklet.js').readlines())
   bookmarklet = re.compile('\s').sub('', bookmarklet)
   return bookmarklet
