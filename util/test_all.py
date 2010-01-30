a885635bcbbb3ef32de6c6d9b981321a396a33c4
John Tantalo <john.tantalo@gmail.com>
2010-01-29 22:02:16 -0800

    added twitter to test_all

--- util/test_all.py
+++ util/test_all.py
@@ -1,7 +1,8 @@
+from test_blogsearch import *
 from test_html import *
 from test_oauth import *
 from test_suggest import *
-from test_blogsearch import *
+from test_twitter import *
 
 if __name__ == "__main__":
   unittest.main()
