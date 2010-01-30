aab70d654334f0e70a6b7ed8353a501e4cc26ea6
John Tantalo <john.tantalo@gmail.com>
2010-01-29 21:34:41 -0800

    stub methods revert to no-op when in production

--- util/stubs.py
+++ util/stubs.py
@@ -1,12 +1,18 @@
 from google.appengine.api import apiproxy_stub_map, urlfetch_stub
 from google.appengine.api.memcache import memcache_stub
 
+from megaera import env
+
 def urlfetch():
+  if not env.is_dev():
+    return
   apiproxy_stub_map.apiproxy = apiproxy_stub_map.APIProxyStubMap() 
   apiproxy_stub_map.apiproxy.RegisterStub('urlfetch', 
     urlfetch_stub.URLFetchServiceStub())
   
 def memcache():
+  if not env.is_dev():
+    return
   apiproxy_stub_map.apiproxy.RegisterStub('memcache', 
     memcache_stub.MemcacheServiceStub())
 
