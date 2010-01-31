a51ad9fecc48fa13437635dca13907073d9df8ea
John Tantalo <john.tantalo@gmail.com>
2010-01-31 15:46:31 -0800

    removed cruft

--- test/mocks/handler_mock.py
+++ test/mocks/handler_mock.py
@@ -1,7 +1,4 @@
-import sys
-
 from util.emend import Emend
-from util.megaera.megaera import NotFoundException
 
 from google.appengine.ext.webapp import Request, Response
 
@@ -12,11 +9,4 @@ def mock_handler(page, request='/', **response):
   handler.response_dict(**response)
   handler.logout_url = lambda self: None
   handler.login_url = lambda self: None
-  def mock_not_found():
-    raise NotFoundException()
-  handler.not_found = mock_not_found
-  def mock_handle_error():
-    (error_type, error, tb) = sys.exc_info()
-    raise error
-  handler.handle_error = mock_handle_error
   return handler
