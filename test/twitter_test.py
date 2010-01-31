f59c6e98fcc1e29090b209b7d7d925e604138b56
John Tantalo <john.tantalo@gmail.com>
2010-01-31 15:25:23 -0800

    added twitter.untweet, modified test to destroytest tweet

---
+++ test/twitter_test.py
@@ -0,0 +1,22 @@
+import unittest
+from time import time
+
+import stubs
+from twitter import tweet, untweet
+
+
+class TestTwitter(unittest.TestCase):
+  def setUp(self):
+    stubs.all()
+
+  def test_tweet_untweet(self):
+    status = "test %s" % int(time())
+    status_id = tweet(status=status)
+    self.assertTrue(status_id > 0)
+    if status_id > 0:
+      destroyed_status_id = untweet(status_id=status_id)
+      self.assertEqual(status_id, destroyed_status_id)
+
+
+if __name__ == "__main__":
+  unittest.main()
--- util/twitter.py
+++ util/twitter.py
@@ -1,4 +1,4 @@
-from megaera import local
+from megaera import local, json
 from oauth import signed_url
 
 from google.appengine.api import urlfetch
@@ -8,8 +8,24 @@ def tweet(status, **credentials):
   if not credentials:
     # shortcut for no-credentials case
     credentials = local.config_get('twitter')
-  update_url = "http://twitter.com/statuses/update.xml"
+  update_url = "http://twitter.com/statuses/update.json"
   fetch_url = signed_url(url=update_url, method='POST', status=status, **credentials)
   response = urlfetch.fetch(fetch_url, method=urlfetch.POST)
-  if response:
-    return response.content
+  try:
+    content = json.read(response.content)
+    return content.get('id')
+  except json.ReadException:
+    pass
+
+def untweet(status_id, **credentials):
+  if not credentials:
+    # shortcut for no-credentials case
+    credentials = local.config_get('twitter')
+  destroy_url = "http://twitter.com/statuses/destroy.json"
+  fetch_url = signed_url(url=destroy_url, method='POST', id=status_id, **credentials)
+  response = urlfetch.fetch(fetch_url, method=urlfetch.POST)
+  try:
+    content = json.read(response.content)
+    return content.get('id')
+  except json.ReadException:
+    pass
