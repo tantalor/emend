471839a24c1080b752cc76cd5625e4b1b7a4a4e8
John Tantalo <john.tantalo@gmail.com>
2010-02-13 10:24:57 -0800

    changed to use MegaeraRequestHandler.url_arg()

--- emend/request_handler.py
+++ emend/request_handler.py
@@ -6,7 +6,7 @@ from model.site import Site
 from model.user import User
 
 from megaera import local
-from megaera.request_handler import MegaeraRequestHandler
+from megaera.request_handler import MegaeraRequestHandler, NotFoundException
 
 from google.appengine.api import users, memcache
 from google.appengine.ext import db
@@ -33,9 +33,8 @@ class EmendRequestHandler(MegaeraRequestHandler):
     """Returns the Site object given by the URL."""
     if self.response_dict().site:
       return self.response_dict().site
-    if self._url_args:
-      # based on the regexp in main.py
-      domain = self._url_args[0]
+    domain = self.url_arg(0)
+    if domain:
       key_name = Site.key_name_from_domain(domain)
       site = Site.get_by_key_name(key_name)
       if not site and create_if_missing:
@@ -45,16 +44,16 @@ class EmendRequestHandler(MegaeraRequestHandler):
         self.response_dict(site = site) # for the template
         return site
     if required:
-      raise megaera.NotFoundException("site not found")
+      raise NotFoundException("site not found")
   
   def get_edit(self, required=False):
     """Returns the Edit object given by the URL."""
     if self.response_dict().edit:
       return self.response_dict().edit
     site = self.get_site()
-    if site and self._url_args:
-      # based on the regexp in main.py
-      index = int(self._url_args[1])
+    index = self.url_arg(1)
+    if site and index:
+      index = int(index)
       edits = Edit.all().\
         ancestor(site).\
         filter('index =', index).\
@@ -64,29 +63,28 @@ class EmendRequestHandler(MegaeraRequestHandler):
         self.response_dict(edit = edit) # for the template
         return edit
     if required:
-      raise megaera.NotFoundException("edit not found")
+      raise NotFoundException("edit not found")
   
   def get_user(self, required=False):
     """Returns the User object given by the URL."""
     if self.response_dict().user:
       return self.response_dict().user
-    if self._url_args:
-      # based on the regexp in main.py
-      key = self._url_args[0]
-      email = urllib.unquote(key)
-      key_name = User.key_name_from_email(email)
+    key = self.url_arg(0)
+    if key:
       user = None
       try:
         user = User.get(key)
       except db.BadKeyError:
         pass
       if not user:
+        email = urllib.unquote(key)
+        key_name = User.key_name_from_email(email)
         user = User.get_by_key_name(key_name)
       if user:
         self.response_dict(user = user) # for the template
         return user
     if required:
-      raise megaera.NotFoundException("user not found")
+      raise NotFoundException("user not found")
     
   def twitter_credentials(self):
     """Returns Emend's twitter credentials, if any."""
