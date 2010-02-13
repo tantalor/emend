815b41dd47b9befd48035470a56bced96a45b128
John Tantalo <john.tantalo@gmail.com>
2010-02-12 21:41:46 -0800

    added namespace parameter

--- handlers/admin/memclear.py
+++ handlers/admin/memclear.py
@@ -4,5 +4,6 @@ def get(handler, response):
   if not handler.is_admin():
     return handler.not_found(status=403)
   key = handler.request.get('key')
-  response.old = memcache.get(key)
-  response.new = memcache.delete(key)
+  namespace = handler.request.get('namespace') or None
+  response.old = memcache.get(key=key, namespace=namespace)
+  response.new = memcache.delete(key=key, namespace=namespace)
