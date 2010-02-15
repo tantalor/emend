b031b9d1fb43d212d64fa9f39ca06bb32c43b997
John Tantalo <john.tantalo@gmail.com>
2010-02-14 20:30:51 -0800

    added canonical url caching

--- emend/canonical_url.py
+++ emend/canonical_url.py
@@ -1,4 +1,4 @@
-from google.appengine.api import urlfetch
+from google.appengine.api import urlfetch, memcache
 
 import re
 
@@ -6,12 +6,22 @@ __canonical_re__ = re.compile('<link rel=["\']canonical["\'][^>]*href=["\']([^\'
 
 
 def canonical_url(url):
+  # check cache
+  cache_key = "canonical_url(%s)"
+  cached = memcache.get(cache_key % url)
+  if cached:
+    return cached
+  # fetch conent
   response = urlfetch.fetch(url.replace(' ', '%20'))
   if response:
-    # print response.content
     # decode content
     html = unicode(response.content, 'iso-8859-1')
-    return canonical_url_in_html(html)
+    # extract canonical url
+    _url = canonical_url_in_html(html)
+    if _url:
+      # cache for posterity
+      memcache.set(cache_key, _url)
+      return _url
 
 def canonical_url_in_html(html):
   match = __canonical_re__.search(html)
@@ -19,9 +29,9 @@ def canonical_url_in_html(html):
     return match.group(1)
 
 
-
 if __name__ == '__main__':
   import stubs
   import sys
   stubs.urlfetch()
+  stubs.memcache()
   print canonical_url(sys.argv[1])
