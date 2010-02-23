36aec11b3c9a1bb163c0a58e953d2272193c2227
John Tantalo <john.tantalo@gmail.com>
2010-02-22 22:14:11 -0800

    added prev to search

--- handlers/search/edits.py
+++ handlers/search/edits.py
@@ -1,6 +1,7 @@
 import re
 from emend import Edit
 
+
 PAGE_SIZE = 10
 
 
@@ -9,6 +10,7 @@ def get(handler, response):
   status = handler.request.get('status')
   response.query = query
   response.status = status
+  search_args = dict(status=status)
   # default heading
   if status == "open":
     response.default_heading = "Open edits"
@@ -19,27 +21,43 @@ def get(handler, response):
   # from edit
   from_key = handler.request.get('from')
   if from_key:
-    from_edit = Edit.get(from_key)
-  else:
-    from_edit = None
-  # get some edits
+    response.to_key = from_key
+    search_args['from_edit'] = Edit.get(from_key)
+  # to edit
+  to_key = handler.request.get('to')
+  if to_key:
+    search_args['to_edit'] = Edit.get(to_key)
+  # search edits
   if re.match('^http://', query):
-    edits = search_by_url(query, from_edit=from_edit, status=status)
+    edits = search_by_url(query, **search_args)
   else:
-    edits = search_by_query(query, from_edit=from_edit, status=status)
+    edits = search_by_query(query, **search_args)
+  # check for next/prev
+  if to_key:
+    if len(edits) > PAGE_SIZE + 1:
+      response.to_key = edits[1].key()
+      edits = edits[1:]
+  if len(edits) > PAGE_SIZE:
+    response.from_key = edits[PAGE_SIZE].key()
   # for output
   response.edits = edits[:PAGE_SIZE]
-  # check for more results  
-  if len(edits) > PAGE_SIZE:
-    response.has_next = 1
-    response.next_from = edits[PAGE_SIZE].key()
 
 
-def search(query, from_edit=None, status=None):
+def search(query, from_edit=None, to_edit=None, status=None):
+  # filter
   if status:
     query = query.filter('status =', status)
+  # order
+  if to_edit:
+    query = query.\
+      order('created').\
+      filter('created >=', to_edit.created)
+    edits = query.fetch(PAGE_SIZE+2)
+    # reverse so we get descending order
+    return edits[::-1]
   if from_edit:
-    query = query.order('-created').\
+    query = query.\
+      order('-created').\
       filter('created <=', from_edit.created)
   else:
     query = query.order('-created')
