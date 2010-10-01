98c4546180b227930f51636f250090a38e6de79d
John Tantalo <john.tantalo@gmail.com>
2010-10-01 09:17:52 -0700

    use console instead

--- handlers/admin/memclear.py
+++
@@ -1,9 +0,0 @@
-from google.appengine.api import memcache
-
-def get(handler, response):
-  if not handler.is_admin():
-    return handler.not_found(status=403)
-  key = handler.request.get('key')
-  namespace = handler.request.get('namespace') or None
-  response.old = memcache.get(key=key, namespace=namespace)
-  response.new = memcache.delete(key=key, namespace=namespace)
--- handlers/admin/memflush.py
+++
@@ -1,6 +0,0 @@
-from google.appengine.api import memcache
-
-def get(handler, response):
-  if not handler.is_admin():
-    return handler.not_found(status=403)
-  response.status = memcache.flush_all()
