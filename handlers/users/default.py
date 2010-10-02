7755f154b96e08d3415c083c4382d12593cae56c
John Tantalo <john.tantalo@gmail.com>
2010-10-01 23:57:55 -0700

    paginated users list

--- handlers/users/default.py
+++ handlers/users/default.py
@@ -1,12 +1,42 @@
 from emend import User
 
+
+PAGE_SIZE = 10
+
+
 def get(handler, response):
-  response.users = User.all().\
-    filter('open >', 0).\
-    order('-open').\
-    fetch(10)
-  response.users += User.all().\
-    filter('open =', 0).\
-    filter('closed >', 0).\
-    order('-closed').\
-    fetch(10)
+  from_key = handler.request.get('from')
+  if from_key:
+    from_user = User.get(from_key)
+    # get users with same open & closed count, order by key
+    users = User.all().\
+      filter('open =', from_user.open).\
+      filter('closed =', from_user.closed).\
+      filter('__key__ >=', from_user.key()).\
+      order('__key__').\
+      fetch(PAGE_SIZE+1)
+    # get more users with same open count and lower closed count,
+    # order by closed count
+    if len(users) < PAGE_SIZE+1:
+      users += User.all().\
+        filter('open =', from_user.open).\
+        filter('closed <', from_user.closed).\
+        order('-closed').\
+        fetch(PAGE_SIZE+1-len(users))
+    # get more users with lower open count,
+    # order by open & closed count
+    if len(users) < PAGE_SIZE+1:
+      users += User.all().\
+        filter('open <', from_user.open).\
+        order('-open').\
+        order('-closed').\
+        fetch(PAGE_SIZE+1-len(users))
+  else:
+    users = User.all().\
+      order('-open').\
+      order('-closed').\
+      fetch(PAGE_SIZE+1)
+  
+  response.users = users[:PAGE_SIZE]
+  if len(users) > PAGE_SIZE:
+    response.from_key = users[PAGE_SIZE].key()
