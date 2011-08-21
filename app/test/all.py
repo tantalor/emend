a27a6f516a2d52f442eb0157641dfa07a2e96a8a
John Tantalo <john.tantalo@gmail.com>
2011-08-20 20:02:44 -0700

    only import as main

--- app/test/all.py
+++ app/test/all.py
@@ -1,15 +1,14 @@
-from bitly_test import *
-from blogsearch_test import *
-from canonical_url_test import *
-from edit_test import *
-from homepage_test import *
-from html_test import *
-from oauth_test import *
-from site_test import *
-from suggest_test import *
-from twitter_test import *
-from user_test import *
-
 if __name__ == '__main__':
+  from bitly_test import *
+  from blogsearch_test import *
+  from canonical_url_test import *
+  from edit_test import *
+  from homepage_test import *
+  from html_test import *
+  from oauth_test import *
+  from site_test import *
+  from suggest_test import *
+  from twitter_test import *
+  from user_test import *
   import unittest
   unittest.main()
