a49cc6bc0012559c5c81aef03b9e6ebc9e30ce3e
John Tantalo <john.tantalo@gmail.com>
2011-09-20 20:23:40 -0700

    added reverse pagination

--- app/handlers/sites/default.py
+++ app/handlers/sites/default.py
@@ -2,14 +2,42 @@ from emend import Site
 PAGE_SIZE = 10
 
 def get(handler, response):
-  # get some sites
   from_domain = handler.request.get('from')
-  from_key_name = Site.key_name_from_domain(from_domain)
-  from_site = Site.get_by_key_name(from_key_name)
-  if from_site:
+  to_domain = handler.request.get('to')
+  
+  from_site, to_site = None, None
+  
+  if to_domain:
+    to_key_name = Site.key_name_from_domain(to_domain)
+    to_site = Site.get_by_key_name(to_key_name)
+  
+  if from_domain:
+    from_key_name = Site.key_name_from_domain(from_domain)
+    from_site = Site.get_by_key_name(from_key_name)
+  
+  # fetch sites
+  if to_site:
+    sites = Site.all().\
+      filter('open =', to_site.open).\
+      filter('domain <=', to_site.domain).\
+      order('domain').\
+      fetch(PAGE_SIZE+2)
+    if len(sites) < PAGE_SIZE+2:
+      pad = Site.all().\
+        filter('open >', to_site.open).\
+        order('open').\
+        order('-domain').\
+        fetch(PAGE_SIZE+2-len(sites))
+      pad.reverse()
+      sites = pad+sites
+    if len(sites) > PAGE_SIZE+1:
+      response.sites = sites[1:PAGE_SIZE+1]
+    else:
+      response.sites = sites[:PAGE_SIZE]
+  elif from_site:
     sites = Site.all().\
       filter('open =', from_site.open).\
-      filter('domain >=', from_domain).\
+      filter('domain >=', from_site.domain).\
       order('domain').\
       fetch(PAGE_SIZE+1)
     if len(sites) < PAGE_SIZE+1:     
@@ -18,14 +46,27 @@ def get(handler, response):
         order('-open').\
         order('domain').\
         fetch(PAGE_SIZE+1-len(sites))
+    response.sites = sites[:PAGE_SIZE]
   else:
     sites = Site.all().\
       order('-open').\
       order('domain').\
       fetch(PAGE_SIZE+1)
-  # for output
-  response.sites = sites[:PAGE_SIZE]
+    response.sites = sites[:PAGE_SIZE]
   
-  if len(sites) > PAGE_SIZE:
-    response.next.domain = sites[PAGE_SIZE].domain
+  # pagination
+  if to_site:
+    response.next.domain = to_site.domain
     response.next.url = "http://%s/sites?from=%s" % (handler.host(), response.next.domain)
+    
+    if len(sites) > PAGE_SIZE+1:
+      response.previous.domain = sites[1].domain
+      response.previous.url = "http://%s/sites?to=%s" % (handler.host(), response.previous.domain)
+  else:
+    if from_site:
+      response.previous.domain = from_site.domain
+      response.previous.url = "http://%s/sites?to=%s" % (handler.host(), response.previous.domain)
+  
+    if len(sites) > PAGE_SIZE:
+      response.next.domain = sites[PAGE_SIZE].domain
+      response.next.url = "http://%s/sites?from=%s" % (handler.host(), response.next.domain)
