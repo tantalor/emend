da179dfbc4f78872779e1b046041ff5aa1a924b8
John Tantalo <john.tantalo@gmail.com>
2011-12-12 22:13:17 -0800

    changed to python27, added jinja filters

--- app/main.py
+++ app/main.py
@@ -4,6 +4,17 @@ from google.appengine.ext.webapp import WSGIApplication
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 from emend import RequestHandler
+from emend.diff import diff, diff_src, diff_dst
+from rfc3339 import datetimetostr as rfc3339
+from urllib import quote
+from megaera import get_jinja2_env
+
+jinja2_env = get_jinja2_env()
+jinja2_env.filters['quote'] = lambda s: quote(s.encode('utf8'))
+jinja2_env.globals['diff'] = diff
+jinja2_env.globals['diff_src'] = diff_src
+jinja2_env.globals['diff_dst'] = diff_dst
+jinja2_env.filters['rfc3339'] = rfc3339
 
 def routes():
   return yaml.load(file('routes.yaml'))
@@ -14,11 +25,4 @@ def handlers():
     for (path, page) in routes()
   ] if handler]
 
-def application():
-  return WSGIApplication(handlers(), debug=True)
-
-def main():
-  run_wsgi_app(application())
-
-if __name__ == "__main__":
-  main()
+application = WSGIApplication(handlers(), debug=True)
