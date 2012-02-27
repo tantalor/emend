10fdb596546553eb82fb5ced94aac9e7cfb1bbc0
John Tantalo <john.tantalo@gmail.com>
2012-02-26 21:20:09 -0800

    added navigation urls

--- app/handlers/search/default.py
+++ app/handlers/search/default.py
@@ -1,2 +1,5 @@
 def get(handler, response):
-  pass
+  search_url = handler.urlize("/search/edits")
+  response.search.by_query.url = search_url+"?query=%s"
+  response.search.by_url.url = search_url+"?url=%s"
+  response.search.by_url_sha1.url = search_url+"?url_sha1=%s"
