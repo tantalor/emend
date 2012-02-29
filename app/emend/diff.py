c9fc890c737fd4a767f2212c45c33b61fc240c32
John Tantalo <john.tantalo@gmail.com>
2012-02-28 20:40:32 -0800

    changed to use `ins` and `del` tags instead of `span`

--- app/emend/diff.py
+++ app/emend/diff.py
@@ -1,17 +1,21 @@
 from difflib import SequenceMatcher
 
+def insert(s):
+  return '<ins style="background-color:#ada;text-decoration:none">%s</ins>' % s
+
+def delete(s):
+  return '<del style="background-color:#faa;text-decoration:none">%s</del>' % s
+
 def diff(src, dst):
   def ops():
     seq = SequenceMatcher(None, src, dst)
     for (op, i1, i2, j1, j2) in seq.get_opcodes():
       if op[0] == 'd':
-        yield '<span style="background-color:#faa">%s</span>' % src[i1:i2]
+        yield delete(src[i1:i2])
       elif op[0] == 'i':
-        yield '<span style="background-color:#ada">%s</span>' % dst[j1:j2]
+        yield insert(dst[j1:j2])
       elif op[0] == 'r':
-        yield '<span style="background-color:#faa">%s</span>'\
-              '<span style="background-color:#ada">%s</span>'\
-              % (src[i1:i2], dst[j1:j2])
+        yield delete(src[i1:i2])+insert(dst[j1:j2])
       else:
         yield src[i1:i2]
   return ''.join(s for s in ops())
@@ -32,10 +36,10 @@ def direct_diff(src, dst, invert=False):
   def span(subject, op, i, j):
     if op == 'equal':
       return subject[i:j]
+    elif invert:
+      return insert(subject[i:j])
     else:
-      return '<span style="background-color:#%s">%s</span>' % (
-        'ada' if invert else 'faa',
-        subject[i:j])
+      return delete(subject[i:j])
   # get opcodes and subject string (src or dst)
   subject, opcodes = ops(src, dst)
   # map opcodes to a list of spans (.same or .different)
